e4257f557cf9ae127c0850eae9dfc3ae31c4b825
Karssou <alexandrelarue44@gmail.com>
2025-11-07 22:33:02 +0100

    Import of NUXT UI, tailwindcss, color-mode and other [ADD] : Creation of landing page for NOHAM THE BEST <3

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,18 @@
+<script lang="ts" setup>
+const count = ref(0);
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <section class="w-screen h-screen flex flex-col justify-center items-center">
+    <h1 class="text-6xl font-bold m-2">Bienvenue Noham</h1>
+    <p class="text-green-400">
+      Installe toi et prends ton clavier pour admirer la beauté de notre
+      invention
+    </p>
+    <div class="mt-5 flex justify-center items-center gap-5">
+      <UButton icon="i-lucide-minus" size="sm" @click="count--" />
+      <span>{{ count }}</span>
+      <UButton icon="i-lucide-plus" size="sm" @click="count++" />
+    </div>
+  </section>
 </template>
