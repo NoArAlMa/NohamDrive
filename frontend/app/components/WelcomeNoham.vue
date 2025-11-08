8ffcdb778d5611f3f5a22d6fbb1e3ec7e500f712
Karssou <alexandrelarue44@gmail.com>
2025-11-08 13:12:40 +0100

    [UPDATE] Updating landing page for Noham

--- frontend/app/components/WelcomeNoham.vue
+++ frontend/app/components/WelcomeNoham.vue
@@ -4,15 +4,27 @@ const count = ref(0);
 
 <template>
   <section class="w-screen h-screen flex flex-col justify-center items-center">
-    <h1 class="text-6xl font-bold m-2">Bienvenue Noham</h1>
-    <p class="text-green-400">
+    <h1 class="text-6xl font-bold m-2 shadow-lg">Bienvenue Noham</h1>
+    <p class="text-green-300 shadow">
       Installe toi et prends ton clavier pour admirer la beauté de notre
       invention
     </p>
     <div class="mt-5 flex justify-center items-center gap-5">
-      <UButton icon="i-lucide-minus" size="sm" @click="count--" />
-      <span>{{ count }}</span>
-      <UButton icon="i-lucide-plus" size="sm" @click="count++" />
+      <UButton
+        icon="i-lucide-minus"
+        variant="outline"
+        size="sm"
+        @click="count--"
+      />
+      <span :class="count < 0 ? 'text-red-600' : 'text-white'">
+        {{ count }}
+      </span>
+      <UButton
+        variant="outline"
+        icon="i-lucide-plus"
+        size="sm"
+        @click="count++"
+      />
     </div>
   </section>
 </template>
