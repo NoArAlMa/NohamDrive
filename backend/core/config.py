9b8992b2170027097a98f0bf3823a8402f0882d8
Alexandre LARUE <68859290+Karssou@users.noreply.github.com>
2025-11-12 18:30:04 +0100

    Alembic init (#19)
    * [ADD] Added alembic to the requirements.txt
    
    * [ADD] Init alembic + modified variable environment for backend (parsing DB infos)
    
    * [UPDATE] Alembic partially connected with SQLModel (already compatible), also modified settings.py to accept new db_info parsing
    
    * [UPDATE] Alembic fully initalized & Engine database SQLModel created

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,11 +1,35 @@
+# Import d'une classe pydantic pour gérer les .env
 from pydantic_settings import BaseSettings
 
+# Classe qui va nous permettre d'accéder à notre .env
+
 class Settings(BaseSettings):
-    database_url: str
+    
+    # Database
+    
+    DB_NAME: str
+    DB_USER: str
+    DB_HOST: str
+    DB_PASSWORD: str
+    
+    # Token 
     secret_key: str
-    algorithm: str
+    algorithm: str = "HS256"
+    
+    #Developemment
+    DEBUG : bool
    
     class Config:
+        # Le fichier qu'il doit lire
         env_file = ".env"
+        env_file_encoding = "utf-8"
+        
+        
+ 
+    @property
+    def database_url(self):
+        """Génère l'URL de connexion à la base de données."""
+        return f"postgresql+psycopg2://{self.db_user}:{self.db_password}@{self.db_host}:5432/{self.db_name}"
+
 
 settings = Settings()
