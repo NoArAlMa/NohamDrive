c21d903aa7ebaf593661e633d3bda4258eb60662
Karssou <alexandrelarue44@gmail.com>
2025-11-12 14:27:42 +0100

    [UPDATE] Alembic partially connected with SQLModel (already compatible), also modified settings.py to accept new db_info parsing

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -25,6 +25,9 @@ target_metadata = None
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+from sqlmodel import SQLModel
+#Importer le engine
+
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -50,24 +53,14 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
+def run_migrations_online():
+    connectable = engine
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=SQLModel.metadata,
+            compare_type=True,
         )
-
         with context.begin_transaction():
             context.run_migrations()
 
--- backend/core/config.py
+++ backend/core/config.py
@@ -1,11 +1,23 @@
+# Import d'une classe pydantic pour gérer les .env
 from pydantic_settings import BaseSettings
 
+# Classe qui va nous permettre d'accéder à notre .env
 class Settings(BaseSettings):
-    database_url: str
+    
+    # Database
+    
+    db_name: str
+    db_user: str
+    db_host: str
+    db_port: str
+    
+    # Token 
     secret_key: str
     algorithm: str
    
     class Config:
+        # Le fichier qu'il doit lire
         env_file = ".env"
+
 
 settings = Settings()
