a82fb595e341c22ec9ac4c91b444c541312a90c6
Karssou <alexandrelarue44@gmail.com>
2025-12-08 17:22:59 +0100

    [ADD] Implement initial frontend structure with Vue components and Pinia integration

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,11 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-  <UApp class="min-h-screen p-4">
-    <section class="flex justify-center items-center h-screen">
-      <div class="max-w-[80%] w-full h-[80vh]">
-        <FileExplorerContainer />
-      </div>
-    </section>
+  <UApp>
+    <WelcomeNoham />
   </UApp>
 </template>
