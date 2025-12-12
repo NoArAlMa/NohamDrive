beeae9d396ad8f7954ad507e9582383672944ab5
Karssou <alexandrelarue44@gmail.com>
2025-12-12 15:16:28 +0100

    [ADD] Created authentication routes and middleware; refactor environment variables and enhance frontend configuration

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -8,4 +8,11 @@ export default defineNuxtConfig({
   // Importation des fichiers css principaux
 
   css: ["~/assets/css/main.css"],
+
+  // Configuration du l'environnement
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
+    },
+  },
 });
