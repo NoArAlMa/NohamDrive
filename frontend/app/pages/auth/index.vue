001332503b455356bc95eac990bf0703b4514b41
Karssou <alexandrelarue44@gmail.com>
2026-02-11 13:38:30 +0100

    blablabla

--- frontend/app/pages/auth/index.vue
+++ frontend/app/pages/auth/index.vue
@@ -2,21 +2,9 @@
 const route = useRoute();
 const router = useRouter();
 
-const mode = computed<"login" | "register">({
-  get() {
-    return route.query.mode === "register" ? "register" : "login";
-  },
-  set(value) {
-    router.replace({
-      query: {
-        ...route.query,
-        mode: value === "login" ? undefined : "register",
-      },
-    });
-  },
-});
+const mode = ref<"login" | "register">("login");
 </script>
-
+²
 <template>
   <section class="min-h-screen flex justify-center items-center">
     <AuthForm v-model:mode="mode" class="grow" />
