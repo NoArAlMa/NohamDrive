a07f8a804c95a6842c981fca7a8bbe16e28ebea7
Alexandre LARUE <68859290+Karssou@users.noreply.github.com>
2026-02-12 12:28:09 +0100

    UI authentification [PROTOTYPE] (#71)
    * [ADD] Create of first prototype of the AuthForm
    
    * [ADD] Add zod dependency
    [UPDATE] Run NPM Update
    
    * [ADD] Include zod to the AuthForm
    
    [ADD] Add smooth transition to the AuthForm from login to create account
    
    * [ADD] Add `Auth` page
    [UPDATE] Update `AuthForm`
    
    * blablabla

---
+++ frontend/app/pages/auth/index.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const route = useRoute();
+const router = useRouter();
+
+const mode = ref<"login" | "register">("login");
+</script>
+²
+<template>
+  <section class="min-h-screen flex justify-center items-center">
+    <AuthForm v-model:mode="mode" class="grow" />
+  </section>
+</template>
