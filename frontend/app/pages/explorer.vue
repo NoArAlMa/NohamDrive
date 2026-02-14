83ab5a69b7bd0cdbe1bb5e2a8f816e226fc4b24a
Karssou <alexandrelarue44@gmail.com>
2026-02-14 14:31:56 +0100

    [UPDATE] Little modification of `fileTree` store [ADD] Add an `explorer page`

---
+++ frontend/app/pages/explorer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section
+    class="w-full my-auto mx-auto max-w-[80%] h-screen flex justify-center items-center"
+  >
+    <FileExplorerContainer class="max-w-7xl max-h-[80%] w-full" />
+  </section>
+</template>
