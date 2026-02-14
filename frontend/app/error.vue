07b3094b20f103fc24b963ebff4f90021ecf23fa
Alexandre LARUE <68859290+Karssou@users.noreply.github.com>
2026-02-14 14:12:37 +0100

    [ADD] Add a error page on frontend (#74)
    [FIX] Comment `DB_DNS` state variable which made the config bug
    [ADD] Add variable `DB_HOST` to `.env.example`

---
+++ frontend/app/error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps<{
+  error: NuxtError;
+}>();
+</script>
+
+<template>
+  <UError
+    :error="error"
+    :ui="{
+      statusCode: 'text-5xl font-bold text-primary',
+      statusMessage: 'text-2xl font-medium text-neutral',
+    }"
+  />
+</template>
