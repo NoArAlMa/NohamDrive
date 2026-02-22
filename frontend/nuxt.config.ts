58c62628df14cb977eb9f644d50e12b828a23dcc
Karssou <alexandrelarue44@gmail.com>
2026-02-22 04:43:40 +0100

    God help me pls =

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -27,6 +27,17 @@ export default defineNuxtConfig({
     transpile: ["vue-files-preview"],
   },
 
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+      cssnano:
+        process.env.NODE_ENV === "production"
+          ? { preset: ["default", { discardComments: { removeAll: true } }] }
+          : false, // disable cssnano when not in production
+    },
+  },
+
   $production: {
     ssr: false,
     nitro: {
