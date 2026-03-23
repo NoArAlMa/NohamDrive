5582d3a137226b71204928ca3f55b66c55b6a13b
Alexandre LARUE <68859290+Karssou@users.noreply.github.com>
2026-03-23 18:19:10 +0100

    [UPDATE] Update auth-form (#101)

---
+++ frontend/app/components/Auth/Register.vue
@@ -0,0 +1,219 @@
+<script lang="ts" setup>
+import type { AuthFormField, FormSubmitEvent, StepperItem } from "@nuxt/ui";
+import * as z from "zod";
+
+const authForm = useTemplateRef("authForm");
+const generalError = ref<string | null>(null);
+const step = ref(0);
+
+const items: StepperItem[] = [
+  {
+    title: "Informations",
+    icon: "material-symbols:person-outline-rounded",
+  },
+  {
+    title: "Sécurité",
+    icon: "material-symbols:lock-outline",
+  },
+];
+
+const steps = [
+  {
+    id: 0,
+    title: "Account",
+    fields: ["name", "username", "email"],
+  },
+  {
+    id: 1,
+    title: "Security",
+    fields: ["password", "password_confirmation"],
+  },
+];
+
+const isLastStep = computed(() => step.value === steps.length - 1);
+
+const registerFields: AuthFormField[] = [
+  {
+    name: "name",
+    type: "text",
+    label: "Full Name",
+    placeholder: "Enter your name",
+    required: true,
+  },
+  {
+    name: "username",
+    type: "text",
+    label: "Username",
+    placeholder: "Enter a username",
+    required: true,
+  },
+  {
+    name: "email",
+    type: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+    required: true,
+  },
+  {
+    name: "password",
+    type: "password",
+    label: "Password",
+    placeholder: "Choose a password",
+    required: true,
+  },
+  {
+    name: "password_confirmation",
+    type: "password",
+    label: "Confirm password",
+    placeholder: "Confirm your password",
+    required: true,
+  },
+];
+
+const currentFields = computed(() => {
+  return registerFields.filter((field) =>
+    steps[step.value]!.fields.includes(field.name),
+  );
+});
+
+const stepSchemas = [
+  z.object({
+    name: z
+      .string("Full name required")
+      .min(3, "Must be at least 3 characters"),
+    username: z
+      .string("Username required")
+      .min(3, "Must be at least 3 characters"),
+    email: z.email("Invalid email"),
+  }),
+  z
+    .object({
+      password: z
+        .string("Password is required")
+        .min(8, "Must be at least 8 characters"),
+      password_confirmation: z
+        .string("Please confirm your password")
+        .min(8, "Must be at least 8 characters"),
+    })
+    .refine((data) => data.password === data.password_confirmation, {
+      message: "Passwords do not match",
+      path: ["password_confirmation"],
+    }),
+];
+
+const fullData = reactive<any>({});
+
+async function onSubmit() {
+  generalError.value = null;
+
+  const { password_confirmation, ...cleanedData } = fullData;
+
+  try {
+    await $fetch("/auth/register", {
+      method: "POST",
+      body: cleanedData,
+    });
+  } catch (error: any) {
+    const backend = error?.data;
+
+    if (backend?.statusCode === 422 && backend?.data) {
+      const formattedErrors = Object.entries(backend.data).map(
+        ([name, message]) => ({
+          name,
+          message: String(message),
+        }),
+      );
+
+      authForm.value?.formRef?.setErrors(formattedErrors);
+      generalError.value = backend.message;
+    } else {
+      generalError.value =
+        backend?.message ?? "Une erreur inattendue est survenue";
+    }
+  }
+}
+
+async function handleNext(payload: FormSubmitEvent<any>) {
+  generalError.value = null;
+
+  try {
+    Object.assign(fullData, payload.data);
+
+    // validation du step actuel
+    await stepSchemas[step.value]!.parseAsync(payload.data);
+
+    if (!isLastStep.value) {
+      step.value++;
+      return;
+    }
+
+    await onSubmit();
+  } catch (error: any) {
+    if (error?.issues) {
+      const formattedErrors = error.issues.map((err: any) => ({
+        name: err.path[0],
+        message: err.message,
+      }));
+
+      authForm.value?.formRef?.setErrors(formattedErrors);
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 p-4">
+    <UPageCard class="w-full max-w-md shadow-md">
+      <UStepper
+        v-model="step"
+        :items="items"
+        :disabled="true"
+        class="w-full"
+        size="sm"
+      />
+
+      <UAuthForm
+        ref="authForm"
+        title="Create account"
+        :schema="stepSchemas[step]"
+        :fields="currentFields"
+        @submit="handleNext"
+        :submit="{
+          label: isLastStep ? 'Create account' : 'Next',
+          loadingAuto: true,
+          color: 'primary',
+        }"
+      >
+        <template #description>
+          Already have an account ?
+          <ULink
+            @click="$emit('switch-mode', 'login')"
+            class="text-primary font-medium"
+            >Log in</ULink
+          >.
+        </template>
+        <template #validation>
+          <UAlert
+            v-if="generalError"
+            color="error"
+            variant="subtle"
+            icon="material-symbols:info-outline-rounded"
+            :title="generalError"
+          />
+        </template>
+
+        <template #footer>
+          <div class="flex w-full">
+            <UButton
+              v-if="step > 0"
+              label="Back"
+              @click="step--"
+              variant="ghost"
+              icon="material-symbols:arrow-back-rounded"
+            />
+          </div>
+        </template>
+      </UAuthForm>
+    </UPageCard>
+  </div>
+</template>
